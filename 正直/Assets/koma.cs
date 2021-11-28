ee218c026859c4dc88d86f1c97a29c99b5f570eb
26matsu <ohno564arashi@gmail.com>
2021-11-28 20:22:24 +0900

    ボタン表示、板の座標設定

---
+++ 正直/Assets/koma.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class koma : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+	public void OnClick(int position)
+    {
+        Debug.Log(position);
+    }
+
+    void Update()
+    {
+        
+    }
+}
