7c9c7242ad59bd60108106d8478e2809ee41d6c1
26matsu <ohno564arashi@gmail.com>
2021-12-26 22:08:09 +0900

    コマをゲームオブジェクトにする方法に路線変更した

--- 正直/Assets/koma.cs
+++ 正直/Assets/koma.cs
@@ -65,134 +65,144 @@ public class koma : MonoBehaviour
         komaInfoMe.Add(19, "香車a");
 
 
-        GameObject kyousya1 = GameObject.Find("91").transform.Find("Image");
+        GameObject kyousya1 = GameObject.Find("91Image");
         m_Image = kyousya1.GetComponent<Image>();
         m_Image.sprite = m_Sprite[24];
-        // GameObject keima1 = GameObject.Find ("81");
-        // m_Image = keima1.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[22];
-        // GameObject ginsyou1 = GameObject.Find ("71");
-        // m_Image = ginsyou1.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[20];
-        // GameObject kinsyou1 = GameObject.Find ("61");
-        // m_Image = kinsyou1.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[19];
-        // GameObject gyokusyou = GameObject.Find ("51");
-        // m_Image = gyokusyou.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[1];
-        // GameObject kinsyou2 = GameObject.Find ("41");
-        // m_Image = kinsyou2.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[19];
-        // GameObject ginsyou2 = GameObject.Find ("31");
-        // m_Image = ginsyou2.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[20];
-        // GameObject keima2 = GameObject.Find ("21");
-        // m_Image = keima2.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[22];
-        // GameObject kyousya2 = GameObject.Find ("11");
-        // m_Image = kyousya2.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[24];
+        GameObject keima1 = GameObject.Find ("81Image");
+        m_Image = keima1.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[22];
+        GameObject ginsyou1 = GameObject.Find ("71Image");
+        m_Image = ginsyou1.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[20];
+        GameObject kinsyou1 = GameObject.Find ("61Image");
+        m_Image = kinsyou1.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[19];
+        GameObject gyokusyou = GameObject.Find ("51Image");
+        m_Image = gyokusyou.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[1];
+        GameObject kinsyou2 = GameObject.Find ("41Image");
+        m_Image = kinsyou2.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[19];
+        GameObject ginsyou2 = GameObject.Find ("31Image");
+        m_Image = ginsyou2.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[20];
+        GameObject keima2 = GameObject.Find ("21Image");
+        m_Image = keima2.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[22];
+        GameObject kyousya2 = GameObject.Find ("11Image");
+        m_Image = kyousya2.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[24];
 
-        // GameObject hisya1 = GameObject.Find ("82");
-        // m_Image = hisya1.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[15];
-        // GameObject gakugyou1 = GameObject.Find ("22");
-        // m_Image = gakugyou1.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[17];
+        GameObject hisya1 = GameObject.Find ("82Image");
+        m_Image = hisya1.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[15];
+        GameObject gakugyou1 = GameObject.Find ("22Image");
+        m_Image = gakugyou1.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[17];
 
-        // GameObject hohei1 = GameObject.Find ("93");
-        // m_Image = hohei1.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[26];
-        // GameObject hohei2 = GameObject.Find ("83");
-        // m_Image = hohei2.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[26];
-        // GameObject hohei3 = GameObject.Find ("73");
-        // m_Image = hohei3.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[26];
-        // GameObject hohei4 = GameObject.Find ("63");
-        // m_Image = hohei4.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[26];
-        // GameObject hohei5 = GameObject.Find ("53");
-        // m_Image = hohei5.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[26];
-        // GameObject hohei6 = GameObject.Find ("43");
-        // m_Image = hohei6.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[26];
-        // GameObject hohei7 = GameObject.Find ("33");
-        // m_Image = hohei7.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[26];
-        // GameObject hohei8 = GameObject.Find ("23");
-        // m_Image = hohei8.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[26];
-        // GameObject hohei9 = GameObject.Find ("13");
-        // m_Image = hohei9.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[26];
+        GameObject hohei1 = GameObject.Find ("93Image");
+        m_Image = hohei1.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[26];
+        GameObject hohei2 = GameObject.Find ("83Image");
+        m_Image = hohei2.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[26];
+        GameObject hohei3 = GameObject.Find ("73Image");
+        m_Image = hohei3.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[26];
+        GameObject hohei4 = GameObject.Find ("63Image");
+        m_Image = hohei4.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[26];
+        GameObject hohei5 = GameObject.Find ("53Image");
+        m_Image = hohei5.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[26];
+        GameObject hohei6 = GameObject.Find ("43Image");
+        m_Image = hohei6.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[26];
+        GameObject hohei7 = GameObject.Find ("33Image");
+        m_Image = hohei7.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[26];
+        GameObject hohei8 = GameObject.Find ("23Image");
+        m_Image = hohei8.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[26];
+        GameObject hohei9 = GameObject.Find ("13Image");
+        m_Image = hohei9.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[26];
 
 
-        // GameObject hohei11 = GameObject.Find ("97");
-        // m_Image = hohei11.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[13];
-        // GameObject hohei12 = GameObject.Find ("87");
-        // m_Image = hohei12.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[13];
-        // GameObject hohei13 = GameObject.Find ("77");
-        // m_Image = hohei13.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[13];
-        // GameObject hohei14 = GameObject.Find ("67");
-        // m_Image = hohei14.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[13];
-        // GameObject hohei15 = GameObject.Find ("57");
-        // m_Image = hohei15.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[13];
-        // GameObject hohei16 = GameObject.Find ("47");
-        // m_Image = hohei16.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[13];
-        // GameObject hohei17 = GameObject.Find ("37");
-        // m_Image = hohei17.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[13];
-        // GameObject hohei18 = GameObject.Find ("27");
-        // m_Image = hohei18.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[13];
-        // GameObject hohei19 = GameObject.Find ("17");
-        // m_Image = hohei19.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[13];
+        GameObject hohei11 = GameObject.Find ("97Image");
+        m_Image = hohei11.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[13];
+        GameObject hohei12 = GameObject.Find ("87Image");
+        m_Image = hohei12.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[13];
+        GameObject hohei13 = GameObject.Find ("77Image");
+        m_Image = hohei13.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[13];
+        GameObject hohei14 = GameObject.Find ("67Image");
+        m_Image = hohei14.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[13];
+        GameObject hohei15 = GameObject.Find ("57Image");
+        m_Image = hohei15.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[13];
+        GameObject hohei16 = GameObject.Find ("47Image");
+        m_Image = hohei16.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[13];
+        GameObject hohei17 = GameObject.Find ("37Image");
+        m_Image = hohei17.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[13];
+        GameObject hohei18 = GameObject.Find ("27Image");
+        m_Image = hohei18.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[13];
+        GameObject hohei19 = GameObject.Find ("17Image");
+        m_Image = hohei19.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[13];
 
-        // GameObject gakugyou2 = GameObject.Find ("88");
-        // m_Image = gakugyou2.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[4];
-        // GameObject hisya2 = GameObject.Find ("28");
-        // m_Image = hisya2.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[2];
+        GameObject gakugyou2 = GameObject.Find ("88Image");
+        m_Image = gakugyou2.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[4];
+        GameObject hisya2 = GameObject.Find ("28Image");
+        m_Image = hisya2.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[2];
         
-        // GameObject kyousya3 = GameObject.Find ("99");
-        // m_Image = kyousya3.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[11];
-        // GameObject keima3 = GameObject.Find ("89");
-        // m_Image = keima3.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[9];
-        // GameObject ginsyou3 = GameObject.Find ("79");
-        // m_Image = ginsyou3.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[7];
-        // GameObject kinsyou3 = GameObject.Find ("69");
-        // m_Image = kinsyou3.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[6];
-        // GameObject ousyou = GameObject.Find ("59");
-        // m_Image = ousyou.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[0];
-        // GameObject kinsyou4 = GameObject.Find ("49");
-        // m_Image = kinsyou4.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[6];
-        // GameObject ginsyou4 = GameObject.Find ("39");
-        // m_Image = ginsyou4.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[7];
-        // GameObject keima4 = GameObject.Find ("29");
-        // m_Image = keima4.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[9];
-        // GameObject kyousya4 = GameObject.Find ("19");
-        // m_Image = kyousya4.GetComponent<Image>();
-        // m_Image.sprite = m_Sprite[11];
+        GameObject kyousya3 = GameObject.Find ("99Image");
+        m_Image = kyousya3.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[11];
+        GameObject keima3 = GameObject.Find ("89Image");
+        m_Image = keima3.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[9];
+        GameObject ginsyou3 = GameObject.Find ("79Image");
+        m_Image = ginsyou3.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[7];
+        GameObject kinsyou3 = GameObject.Find ("69Image");
+        m_Image = kinsyou3.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[6];
+        GameObject ousyou = GameObject.Find ("59Image");
+        m_Image = ousyou.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[0];
+        GameObject kinsyou4 = GameObject.Find ("49Image");
+        m_Image = kinsyou4.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[6];
+        GameObject ginsyou4 = GameObject.Find ("39Image");
+        m_Image = ginsyou4.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[7];
+        GameObject keima4 = GameObject.Find ("29Image");
+        m_Image = keima4.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[9];
+        GameObject kyousya4 = GameObject.Find ("19Image");
+        m_Image = kyousya4.GetComponent<Image>();
+        m_Image.sprite = m_Sprite[11];
 
     }
+
+    // public static bool HasChild( this GameObject self )
+    // {
+    //     return 0 < self.transform.childCount;
+    // }
+
+    // public static bool HasChild( this Component self )
+    // {
+    //     return 0 < self.transform.childCount;
+    // }
 
     void moveCheck(int position)
     {
@@ -225,17 +235,30 @@ public class koma : MonoBehaviour
         else{
             komaInfo = komaInfoEnemy;
         }
-        if(!komaInfo.ContainsKey(position) || komaInfo[position] == ""){
+        GameObject kyousyaB = GameObject.Find (tempPosition.ToString() + "Image");
+        
+        GameObject kyousyaA = GameObject.Find (position.ToString());
+        //if(!komaInfo.ContainsKey(position) || komaInfo[position] == ""){
+        if(kyousyaA.transform.childCount == 0){
+            Debug.Log(kyousyaB.transform.childCount);
             isClick = false;
             tempKomaInfo = komaInfo[tempPosition];
             komaInfo[tempPosition] = "";
-            GameObject kyousyaB = GameObject.Find (tempPosition.ToString());
-            m_Image = kyousyaB.GetComponent<Image>();
-            GameObject kyousyaA = GameObject.Find (position.ToString());
-            m_Image2 = kyousyaA.GetComponent<Image>();
-            tempImage = m_Image.sprite;
-            m_Image.sprite = m_Image2.sprite;
-            m_Image2.sprite = tempImage;
+            // m_Image = kyousyaB.GetComponent<Image>();
+
+            GameObject obj = new GameObject(position.ToString() + "Image");
+            obj.transform.parent = kyousyaA.GetComponent<Transform>();
+            obj.AddComponent<Image>();
+            m_Image = obj.GetComponent<Image>();
+            m_Image.sprite = kyousyaB.gameObject.GetComponent<Image>().sprite;
+            Destroy (kyousyaB.GetComponent<Transform>().GetChild(0));
+            // m_Image2 = kyousyaA.GetComponent<Image>();
+            
+            // tempImage = m_Image.sprite;
+            // m_Image.sprite = m_Image2.sprite;
+            // m_Image2.sprite = tempImage;
+            // kyousyaA = kyousyaB;
+            // kyousyaB = null;
 
             komaInfo[position] = tempKomaInfo;
             
