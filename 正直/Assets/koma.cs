7ebff2dc5b9311e8a920eca8611c964536cf304c
26matsu <ohno564arashi@gmail.com>
2022-01-16 21:30:39 +0900

    コマの乗り越え防止制御、飛車と角行の制御実装

--- 正直/Assets/koma.cs
+++ 正直/Assets/koma.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class koma : MonoBehaviour
 {
@@ -15,8 +16,18 @@ public class koma : MonoBehaviour
     Dictionary<int, string> komaInfoEnemy = new Dictionary<int, string>();
     Dictionary<int, string> komaInfoMe = new Dictionary<int, string>();
 
+    void meFlgAram(){
+        if(meFlg){
+            Debug.Log("あなたの番です");
+        }
+        else{
+            Debug.Log("相手の番です");
+        }
+    }
+
     void Start()
     {
+        meFlgAram();
         //キーと値をセットする
         komaInfoEnemy.Add(91, "kyousyaE2");
         komaInfoEnemy.Add(81, "keimaE2");
@@ -66,22 +77,170 @@ public class koma : MonoBehaviour
 
     }
 
-    int komaMoveRoot(int position, Dictionary<int, string> komaInfo){
-        int zachA = tempPosition / 10;
-        int zachB = tempPosition % 10;
-        int zahiA = position / 10;
-        int zahiB = position % 10;
+    int[] komaMoveRoot(int position, Dictionary<int, string> komaInfo){
+        Dictionary<int, string> komaInfoAite;
+
+        if(meFlg){
+            komaInfoAite = komaInfoEnemy;
+        }
+        else{
+            komaInfoAite = komaInfoMe;
+        }
+
+        bool ifFlg1 = true;
+        bool ifFlg2 = true;
+        int[] arr = new int[0];
+
         if(komaInfo[tempPosition].Contains("fuhyou")){
             if(meFlg){
-                return tempPosition - 1;
+                if(komaInfo.ContainsKey(tempPosition - 1) && komaInfo[tempPosition - 1] != ""){
+                    return arr;
+                }
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length -1] = tempPosition - 1;
             }
             else{
-                return tempPosition + 1;
+                if(komaInfo.ContainsKey(tempPosition + 1) && komaInfo[tempPosition + 1] != ""){
+                    return arr;
+                }
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length -1] = tempPosition + 1;
             }
+            return arr;
         }
-        // TODO 他のコマの動き制御を作る＆障害物より先に行く動く位の制御
+
+        if(komaInfo[tempPosition].Contains("hisya")){
+            int tempNum = tempPosition / 10;
+            // 縦移動制御
+            for(int i = 1; i < 9; i++){
+                // 駒を超えないようにするフラグ
+                if(komaInfo.ContainsKey(tempPosition + i) && komaInfo[tempPosition + i] != "" || (tempPosition + i) / 10 != tempNum){
+                    ifFlg1 = false;
+                }
+                if(komaInfo.ContainsKey(tempPosition - i) && komaInfo[tempPosition - i] != "" || (tempPosition - i) / 10 != tempNum){
+                    ifFlg2 = false;
+                }
+
+                if(ifFlg1 && (tempPosition + i) % 10 != 0){
+                    if(komaInfoAite.ContainsKey(tempPosition + i) && komaInfoAite[tempPosition + i] != ""){
+                        ifFlg1 = false;
+                    }
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length -1] = tempPosition + i;
+                }
+                if(ifFlg2 && (tempPosition - i) % 10 != 0){
+                    if(komaInfoAite.ContainsKey(tempPosition - i) && komaInfoAite[tempPosition - i] != ""){
+                        ifFlg2 = false;
+                    }
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length -1] = tempPosition - i;
+                }
+                if(!ifFlg1 && !ifFlg2){
+                    ifFlg1 = !ifFlg1;
+                    ifFlg2 = !ifFlg2;
+                    break;
+                }
+            }
+            // 横移動制御
+            for(int j = 10; j < 90; j = j + 10){
+                if(komaInfo.ContainsKey(tempPosition + j) && komaInfo[tempPosition + j] != ""){
+                    ifFlg1 = false;
+                }
+                if(komaInfo.ContainsKey(tempPosition - j) && komaInfo[tempPosition - j] != ""){
+                    ifFlg2 = false;
+                }
+
+                if(ifFlg1 && (tempPosition + j) / 10 < 10){
+                    if(komaInfoAite.ContainsKey(tempPosition + j) && komaInfoAite[tempPosition + j] != ""){
+                        ifFlg1 = false;
+                    }
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length -1] = tempPosition + j;
+                }
+                if(ifFlg2 && (tempPosition - j) / 10 > 0){
+                    if(komaInfoAite.ContainsKey(tempPosition - j) && komaInfoAite[tempPosition - j] != ""){
+                        ifFlg2 = false;
+                    }
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length -1] = tempPosition - j;
+                }
+                if(!ifFlg1 && !ifFlg2){
+                    break;
+                }
+            }
+            return arr;  
+        }
+
+        if(komaInfo[tempPosition].Contains("kakugyou")){
+            // 移動1制御
+            for(int i = 1; i < 9; i++){
+                int iP = i * 11;
+                // 駒を超えないようにするフラグ
+                if(komaInfo.ContainsKey(tempPosition + iP) && komaInfo[tempPosition + iP] != "" || (tempPosition + iP) % 10 == 0){
+                    ifFlg1 = false;
+                }
+                if(komaInfo.ContainsKey(tempPosition - iP) && komaInfo[tempPosition - iP] != "" || (tempPosition - iP) % 10 == 0){
+                    ifFlg2 = false;
+                }
+
+                if(ifFlg1 && (tempPosition + iP) / 10 < 10){
+                    if(komaInfoAite.ContainsKey(tempPosition + iP) && komaInfoAite[tempPosition + iP] != ""){
+                        ifFlg1 = false;
+                    }
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length -1] = tempPosition + iP;
+                }
+                if(ifFlg2 && (tempPosition - iP) / 10 > 0){
+                    if(komaInfoAite.ContainsKey(tempPosition - iP) && komaInfoAite[tempPosition - iP] != ""){
+                        ifFlg2 = false;
+                    }
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length -1] = tempPosition - iP;
+                }
+                if(!ifFlg1 && !ifFlg2){
+                    ifFlg1 = !ifFlg1;
+                    ifFlg2 = !ifFlg2;
+                    break;
+                }
+            }
+            // 移動2制御
+            for(int j = 1; j < 9; j++){
+                int jP = j * 9;
+                // 駒を超えないようにするフラグ
+                if(komaInfo.ContainsKey(tempPosition + jP) && komaInfo[tempPosition + jP] != "" || (tempPosition + jP) % 10 == 0){
+                    ifFlg1 = false;
+                }
+                if(komaInfo.ContainsKey(tempPosition - jP) && komaInfo[tempPosition - jP] != "" || (tempPosition - jP) % 10 == 0){
+                    ifFlg2 = false;
+                }
+
+                if(ifFlg1 && (tempPosition + jP) / 10 < 10){
+                    if(komaInfoAite.ContainsKey(tempPosition + jP) && komaInfoAite[tempPosition + jP] != ""){
+                        ifFlg1 = false;
+                    }
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length -1] = tempPosition + jP;
+                }
+                if(ifFlg2 && (tempPosition - jP) / 10 > 0){
+                    if(komaInfoAite.ContainsKey(tempPosition - jP) && komaInfoAite[tempPosition - jP] != ""){
+                        ifFlg2 = false;
+                    }
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length -1] = tempPosition - jP;
+                }
+                if(!ifFlg1 && !ifFlg2){
+                    break;
+                }
+            }
+            
+            return arr;  
+        }
+
+        // TODO 他のコマの動き制御を作る
         else{
-            return position;
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length -1] = position;
+            return arr;
         }
     }
 
@@ -94,11 +253,20 @@ public class koma : MonoBehaviour
         else{
             komaInfo = komaInfoEnemy;
         }
+        
         if(komaInfo.ContainsKey(position) && komaInfo[position] != ""){
             isClick = true;
             tempPosition = position;
+            int[] returnValue = komaMoveRoot(position,komaInfo);
             Debug.Log(komaInfo[tempPosition]);
-            Debug.Log("このコマは"+komaMoveRoot(position,komaInfo)+"に置けます");
+            if(returnValue.Length == 0){
+                Debug.Log("このコマは動かせない");
+                isClick = !isClick;
+            }
+            else{
+                Debug.Log("このコマは" + string.Join(",",returnValue) + "に置けます");
+            }
+            
             // TODO ハイライトをする
         }
         else{
@@ -109,6 +277,7 @@ public class koma : MonoBehaviour
     void moveKoma(int position)
     {
         Dictionary<int, string> komaInfo;
+        Dictionary<int, string> komaInfoAite;
         Dictionary<int, string> komaInfoMeTemp;
         Dictionary<int, string> komaInfoEnemyTemp;
         string tempKomaInfo;
@@ -117,12 +286,16 @@ public class koma : MonoBehaviour
 
         if(meFlg){
             komaInfo = komaInfoMe;
+            komaInfoAite = komaInfoEnemy;
         }
         else{
             komaInfo = komaInfoEnemy;
+            komaInfoAite = komaInfoMe;
         }
+
+        int[] returnValue = komaMoveRoot(position,komaInfo);
         
-        if(position == komaMoveRoot(position,komaInfo) && (!komaInfo.ContainsKey(position) || komaInfo[position] == "")){
+        if(Array.Exists(returnValue, x => x == position) && (!komaInfo.ContainsKey(position) || komaInfo[position] == "")){
             
             // Debug.Log();
             isClick = false;
@@ -144,27 +317,25 @@ public class koma : MonoBehaviour
             double moveX = (zachA - zahiA) * 0.76;
             double moveY = (zachB - zahiB) * 0.838;
 
-            Debug.Log(moveX);
-            
             GameObject.Find(komaInfo[tempPosition]).transform.Translate((float)moveX,(float)moveY, 0f);
 
             if(meFlg){
                 // 敵の駒を奪えるとき
-                if(komaInfoEnemy.ContainsKey(position) && komaInfoEnemy[position] != ""){ 
-                    GameObject.Find(komaInfoEnemy[position]).transform.Translate((float)zahiA, 0f, 0f);
-                    GameObject.Find(komaInfoEnemy[position]).transform.Rotate(new Vector3(0, 0, 1), 180);
+                if(komaInfoAite.ContainsKey(position) && komaInfoAite[position] != ""){ 
+                    GameObject.Find(komaInfoAite[position]).transform.Translate((float)zahiA, 0f, 0f);
+                    GameObject.Find(komaInfoAite[position]).transform.Rotate(new Vector3(0, 0, 1), 180);
                     // komaInfoMeTemp[i++] = komaInfoEnemy[position];
-                    komaInfoEnemy[position] = "";
+                    komaInfoAite[position] = "";
                     Debug.Log("駒を取った！");
                 }
             }
             else{
                 //　自分の駒が奪われるとき
-                if(komaInfoMe.ContainsKey(position) && komaInfoMe[position] != ""){ 
-                    GameObject.Find(komaInfoMe[position]).transform.Translate(-10f + (float)zahiA, 0f, 0f);
-                    GameObject.Find(komaInfoMe[position]).transform.Rotate(new Vector3(0, 0, 1), 180);
+                if(komaInfoAite.ContainsKey(position) && komaInfoAite[position] != ""){ 
+                    GameObject.Find(komaInfoAite[position]).transform.Translate(-10f + (float)zahiA, 0f, 0f);
+                    GameObject.Find(komaInfoAite[position]).transform.Rotate(new Vector3(0, 0, 1), 180);
                     // komaInfoEnemyTemp[i++] = komaInfoMe[position];
-                    komaInfoMe[position] = "";
+                    komaInfoAite[position] = "";
                     Debug.Log("駒がとられた！");
                 }
             }
@@ -177,6 +348,7 @@ public class koma : MonoBehaviour
             Debug.Log(komaInfo[position] +":"+ position);
 
             meFlg = !meFlg;
+            meFlgAram();
         }
         else{
             isClick = false;
@@ -187,7 +359,6 @@ public class koma : MonoBehaviour
 	public void OnClick(int position)
     {
         if (!isClick){
-
             moveCheck(position);
             
         }
